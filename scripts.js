0d7048b186e5301fc6c7d0119c3a9a0a75f5a8cb
lkehl247 <153117528+lkehl247@users.noreply.github.com>
2024-11-07 16:55:10 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function showSignUp() {
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('signup').style.display = 'block';
+  }
+  
+  function showLogin() {
+    document.getElementById('signup').style.display = 'none';
+    document.getElementById('login').style.display = 'block';
+  }
+  
